4c6237028be05c408ee4c848ec9cc182f74fb3a2
Adnan Yousuf <70625@hexaware.com>
2022-04-05 14:42:31 +0530

    Learned Packages

--- Java/DemoApp.java
+++ Java/DemoApp.java
@@ -1,14 +1,32 @@
+import calculate.Calculator; //Importing user defined package (calculate) which contains 'Calculator' class 
+import java.util.Scanner;
+
 public class DemoApp{
     public static void main(String ar[]){
         System.out.println("Hello World from Java....");
+        System.out.println(".....................................................");
         DemoApp obj=new DemoApp();
         obj.sayHello();
+        System.out.println(".....................................................");
+        obj.letsAdd();
+        System.out.println(".....................................................");
         sayBye();
+
     }
     public void sayHello(){
-        System.out.println("Hello All...");
+        System.out.println("Hello... Please follow the given steps");
     }
     public static void sayBye(){
         System.out.println("Bye Everybody..");
     }
+    public void letsAdd(){
+        int a, b;
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        a = myObj.nextInt();
+        System.out.println("Enter a number: ");
+        b = myObj.nextInt();
+        Calculator obj = new Calculator();
+        System.out.println("The Sum is: "+obj.add(a,b));
+    } 
 }
