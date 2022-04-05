70f6408366ee6d66587f062203ede587841e6b0a
Adnan Yousuf <70625@hexaware.com>
2022-04-05 14:23:58 +0530

    Learned Packages

--- Java/App.java
+++ Java/App.java
@@ -1,5 +1,12 @@
+import calculate.Calculator;
+
 public class App{
     public static void main(String[] args) {
-        System.out.println("Java World");
+        System.out.println("Hello World");
+        Calculator obj = new Calculator();
+        System.out.println(obj.add(5,2));
+        obj.getmax(5,6);
+
     }
+
 }
